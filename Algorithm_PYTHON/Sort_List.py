f77b02515eec79716ceb700e64c5f90676a0a564
채우 <lala1522@gachon.ac.kr>
2025-09-19 00:02:30 +0900

    sortlist test

--- Algorithm_PYTHON/Sort_List.py
+++ Algorithm_PYTHON/Sort_List.py
@@ -10,11 +10,14 @@ class Solution:
         if not head or not head.next:
             return head
 
+
+        # 링크드 리스트 길이를 먼저 파악
         n = 0
         node = head
         while node:
             n += 1
             node = node.next
+
 
         dummy = ListNode(0, head)
         size = 1
@@ -24,27 +27,25 @@ class Solution:
             curr = dummy.next
 
             while curr:
-                # 왼쪽 리스트(head1) 시작
+                # 노드 분해
                 head1 = curr
-                # head1에서 size만큼 이동하여 잘라내기
-                # split 함수: 시작 노드에서 len만큼 잘라 반환, 나머지의 시작 노드를 리턴
                 head2 = self._split(head1, size)
-                # head2에서 size만큼 잘라 다음 덩어리의 시작을 next_start로 받음
                 next_start = self._split(head2, size)
 
-                # 두 정렬 리스트(head1, head2) 병합
+                # 노드 병합
                 merged_head, merged_tail = self._merge(head1, head2)
 
-                # 이전 구간과 연결
+                # 노드 연결
                 prev.next = merged_head
                 prev = merged_tail
+                # 새로시작
                 curr = next_start
 
-            size <<= 1  # 블록 크기 두 배
+            size <<= 1  # 블록 크기 두 배(비트 연산)
 
         return dummy.next
 
-    def _split(self, head: 'Optional[ListNode]', length: int) -> 'Optional[ListNode]':
+    def _split(self, head: Optional[ListNode], length: int) -> Optional[ListNode]:
         """
         head에서 length개 노드를 남기고 연결을 끊어줌.
         반환값은 '끊긴 뒤 남은 리스트의 시작 노드'.
@@ -55,20 +56,18 @@ class Solution:
             if not head.next:
                 break
             head = head.next
-        # 이제 head가 첫 블록의 마지막 노드
         rest = head.next
         head.next = None
         return rest
 
-    def _merge(self, l1: 'Optional[ListNode]', l2: 'Optional[ListNode]'):
-        """
-        두 오름차순 리스트 l1, l2를 병합하여 (머리, 꼬리) 튜플로 반환.
-        """
+    def _merge(self, l1: Optional[ListNode], l2: Optional[ListNode]):
         dummy = ListNode(0)
         tail = dummy
 
         while l1 and l2:
             if l1.val <= l2.val:
+                # 오른쪽 연산을 먼저해서 li의 튜플을 형성한다
+                # li이 만약 2->4->none이라면 (2->4->None, 4->None)을 가리키지만 실제 저장되는 튜플은 (2참조값, 4참조값)
                 tail.next, l1 = l1, l1.next
             else:
                 tail.next, l2 = l2, l2.next
@@ -91,6 +90,7 @@ if __name__ == "__main__":
     n2.next = n3
     n3.next = n4
 
+    # 원본 linkedlist 출력
     curr = n1
     while curr:
         print(curr.val, end=" -> ")
@@ -98,6 +98,7 @@ if __name__ == "__main__":
     print()
 
 
+    # 정렬한 linkedlist 출력
     sol = Solution()
     sorted_head = sol.sortList(n1)
 
